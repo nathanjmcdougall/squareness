d2fc8484261d7fd1640c181ca65dcb3bee30008c
Nathan McDougall <nmcdougall@tonkintaylor.co.nz>
2022-08-20 11:08:46 +1200

    Precise level calculation
    Previously a cutoff system was used but this was too crude

--- src/arithmetic_levels.py
+++ src/arithmetic_levels.py
@@ -1,15 +1,19 @@
 """A script to manually interogate the basis for discrete levels in the arithmetic squareness curve.
 """
+import numpy as np
+
 from squareness import arithmetic_squareness
 
 nums = list(range(2, 5_000))
 
-# Determined visually
-TIER0_CUTOFF = 0.2
-TIER1_CUTOFF = 0.6
+def print_tier_nums(tier_level: int) -> None:
+    for num in nums:
+        squareness = arithmetic_squareness(num)
+        if np.isclose(squareness,(tier_level-1)*(num/tier_level+1) / (num-1)):
+            print(num, end=', ')
+    print('...', end='\n')
 
-for num in nums:
-    squareness = arithmetic_squareness(num)
-    if TIER0_CUTOFF < squareness < TIER1_CUTOFF:
-        print(num, end=', ')
-print('', end='\n')
+if __name__ == "__main__":
+    print('Which tier would you like to print?')
+    tier = int(input())
+    print_tier_nums(tier)
