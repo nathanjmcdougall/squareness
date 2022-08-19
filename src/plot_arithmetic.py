3df6267e98e96c5f619b318ef5d1b4c0f78df596
Nathan McDougall <nmcdougall@tonkintaylor.co.nz>
2022-08-19 23:25:44 +1200

    Added new graph to include asymptotes

--- src/plot_arithmetic.py
+++ src/plot_arithmetic.py
@@ -6,7 +6,7 @@ import matplotlib.ticker as tck
 from nav import GRAPH_PARENT_PATH
 from squareness import arithmetic_squareness
 
-plt.style.use('calibri18.mplstyle')
+plt.style.use("calibri18.mplstyle")
 
 nums = list(range(2, 5_000))
 squarenesses = [arithmetic_squareness(num) for num in nums]
@@ -14,6 +14,7 @@ squarenesses = [arithmetic_squareness(num) for num in nums]
 xs, ys = nums, squarenesses
 y_min, y_max = 0, 1
 
+# Basic to 5000
 fig, ax = plt.subplots(figsize=(10, 20))
 ax.scatter(xs, ys)
 ax.set_xlabel("Number")
@@ -22,4 +23,17 @@ ax.set_title("Arithmetic Squareness")
 ax.set_ylim(y_min, y_max)
 percent_formatter = tck.PercentFormatter(y_max)
 ax.yaxis.set_major_formatter(percent_formatter)
-plt.savefig(GRAPH_PARENT_PATH / 'arithmetic_squareness_basic_to_5000.png')
+plt.savefig(GRAPH_PARENT_PATH / "arithmetic_squareness_basic_to_5000.png")
+
+# To 5000 but with added asymptote lines
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.scatter(xs, ys)
+ax.set_xlabel("Number")
+ax.set_ylabel("Squareness")
+ax.set_title("Arithmetic Squareness")
+ax.set_ylim(y_min, y_max)
+for idx in range(1, 10):
+    ax.axhline(idx / (idx + 1), ls="--", color="black")
+percent_formatter = tck.PercentFormatter(y_max)
+ax.yaxis.set_major_formatter(percent_formatter)
+plt.savefig(GRAPH_PARENT_PATH / "arithmetic_squareness_to_5000_asymptotes.png")
