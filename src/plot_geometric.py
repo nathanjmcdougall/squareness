e4171a29b499382cbd29cf19b8073f6560880744
Nathan McDougall <nmcdougall@tonkintaylor.co.nz>
2022-08-20 13:00:32 +1200

    normalizing geometric sequence to find gradients

--- src/plot_geometric.py
+++ src/plot_geometric.py
@@ -11,6 +11,7 @@ plt.style.use("calibri18.mplstyle")
 
 nums = list(range(2, 50_000))
 squarenesses = [geometric_squareness(num) for num in nums]
+norm_squarenesses = [geometric_squareness(num)/num for num in nums]
 
 plot_xs = np.linspace(2, 50_000, 1_000)
 
@@ -27,3 +28,14 @@ ax.set_ylim(y_min, y_max)
 percent_formatter = tck.PercentFormatter(y_max)
 ax.yaxis.set_major_formatter(percent_formatter)
 plt.savefig(GRAPH_PARENT_PATH / "geometric_squareness_basic_to_50000.png")
+
+# squareness per $n$ to 50,000 to see gradient of lines in originalk plot
+xs, ys = nums, norm_squarenesses
+ymin, ymax = 0, 0.0025
+fig, ax = plt.subplots(figsize=(20, 20))
+ax.scatter(xs, ys)
+ax.set_xlabel("Number")
+ax.set_ylabel("Squareness per $n$")
+ax.set_title("Geometric Squareness per $n$")
+ax.set_ylim(ymin, ymax)
+plt.savefig(GRAPH_PARENT_PATH / "geometric_squareness_normalized_to_50000.png")
