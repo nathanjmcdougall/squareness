c98fd246a6cdb8d94b7da300635e1bbe421dfb4c
Nathan McDougall <nmcdougall@tonkintaylor.co.nz>
2022-08-20 12:08:43 +1200

    geometric squareness graph

---
+++ src/plot_geometric.py
@@ -0,0 +1,30 @@
+"""Plot geometric squareness on a graph
+"""
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tck
+import numpy as np
+
+from nav import GRAPH_PARENT_PATH
+from squareness import geometric_squareness
+
+plt.style.use("calibri18.mplstyle")
+
+nums = list(range(2, 50_000))
+squarenesses = [geometric_squareness(num) for num in nums]
+
+plot_xs = np.linspace(2, 50_000, 1_000)
+
+xs, ys = nums, squarenesses
+y_min, y_max = 0, 1
+
+# Basic to 5000
+fig, ax = plt.subplots(figsize=(20, 20))
+ax.scatter(xs, ys)
+ax.set_xlabel("Number")
+ax.set_ylabel("Squareness")
+ax.set_title("Geometric Squareness")
+ax.set_ylim(y_min, y_max)
+percent_formatter = tck.PercentFormatter(y_max)
+ax.yaxis.set_major_formatter(percent_formatter)
+plt.savefig(GRAPH_PARENT_PATH / "geometric_squareness_basic_to_50000.png")
+
